87b7baa9d3a9755b1990868d7cd6980d3d222eef
Linggg <bluish@dhcp-206-87-115-93.ubcsecure.wireless.ubc.ca>
2019-03-10 11:16:48 -0700

    ready for frontend

--- hello.py
+++ hello.py
@@ -16,29 +16,13 @@ ingredients = {}
 
 @app.route('/')
 def recipeSearch():
-	url = 'https://api.edamam.com/search?q=chicken' + '&app_id=' + app_id + '&app_key=' + app_key + '&from=0&to=9'
+	url = 'https://api.edamam.com/search?q=chicken' + '&app_id=' + app_id + '&app_key=' + app_key + '&from=0&to=4'
 	# url = 'https://api.edamam.com/search?q=' + food_name + '&app_id=' + app_id + '&app_key=' + app_key
 	r = requests.get(url)
 	results = json.loads(r.text)
-	# return json.dumps(r.json()['hits'])
-
-	for x in r.json()['hits']: # seperate the recipes into different arrays
-		# y = json.dumps((r.json())[x])
-		# json.dumps(x['recipe']['ingredientLines'])
-		# print(x['recipe']['image'])
-		for key,value in x.items():
-			ingredients[key] = value # making the dict x as the index of dict ingredients. Hence, the unhashable error
-			print key
-		# name[x] = x['recipe']['labels']
-		# url[x] = x['recipe']['url']
-		# servings[x] = x['recipe']['yield']
-		# image[x] = x['recipe']['image']
+	recipe_dict = results['hits']
 
 	return '1'
-
-	#'https://stackoverflow.com/questions/32911336/what-is-the-difference-between-json-dumps-and-json-load'
-	# difference between json.load and json.dump
-
 
 if __name__ == '__main__':
 	app.run()
