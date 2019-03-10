25f472fa69ddcd07848cb1db867f1ce089e8d100
Linggg <bluish@dhcp-206-87-115-93.ubcsecure.wireless.ubc.ca>
2019-03-10 12:55:04 -0700

    recipe page not working

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import json
 import requests
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_paginate import Pagination, get_page_parameter
 
 
@@ -13,16 +13,14 @@ ingredients = {}
 # servings = {}
 # image = {}
 
-
 @app.route('/')
 def recipeSearch():
 	url = 'https://api.edamam.com/search?q=chicken' + '&app_id=' + app_id + '&app_key=' + app_key + '&from=0&to=4'
 	# url = 'https://api.edamam.com/search?q=' + food_name + '&app_id=' + app_id + '&app_key=' + app_key
 	r = requests.get(url)
-	results = json.loads(r.text)
-	recipe_dict = results['hits']
+	results = r.json()
 
-	return '1'
+	return render_template("recipe.html", recipes=results['hits'])
 
 if __name__ == '__main__':
 	app.run()
