b9ac645c1f01dcf79faf4be9f2b032196c62393d
Linggg <bluish@dhcp-206-87-115-93.ubcsecure.wireless.ubc.ca>
2019-03-10 10:19:18 -0700

    trying to fix dict unhashable problem but unsucessful

--- hello.py
+++ hello.py
@@ -7,11 +7,11 @@ from flask_paginate import Pagination, get_page_parameter
 app = Flask(__name__)
 app_id = '9c6002af'
 app_key = '29d5731c89d6adc4fbba5a8e4bbfb1d9'
-ingredients = set()
-name = set()
-url = set()
-servings = set()
-image = set()
+ingredients = {}
+# name = {}
+# url = {}
+# servings = {}
+# image = {}
 
 
 @app.route('/')
@@ -19,20 +19,22 @@ def recipeSearch():
 	url = 'https://api.edamam.com/search?q=chicken' + '&app_id=' + app_id + '&app_key=' + app_key + '&from=0&to=9'
 	# url = 'https://api.edamam.com/search?q=' + food_name + '&app_id=' + app_id + '&app_key=' + app_key
 	r = requests.get(url)
-	# results = json.loads(r.text)
+	results = json.loads(r.text)
 	# return json.dumps(r.json()['hits'])
 
 	for x in r.json()['hits']: # seperate the recipes into different arrays
 		# y = json.dumps((r.json())[x])
 		# json.dumps(x['recipe']['ingredientLines'])
 		# print(x['recipe']['image'])
-		ingredients[x] = x['recipe']['ingredientLines']
-		name[x] = x['recipe']['labels']
-		url[x] = x['recipe']['url']
-		servings[x] = x['recipe']['yield']
-		image[x] = x['recipe']['image']
+		for key,value in x.items():
+			ingredients[key] = value # making the dict x as the index of dict ingredients. Hence, the unhashable error
+			print key
+		# name[x] = x['recipe']['labels']
+		# url[x] = x['recipe']['url']
+		# servings[x] = x['recipe']['yield']
+		# image[x] = x['recipe']['image']
 
-	return
+	return '1'
 
 	#'https://stackoverflow.com/questions/32911336/what-is-the-difference-between-json-dumps-and-json-load'
 	# difference between json.load and json.dump
