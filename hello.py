69da0051b4b4469068433cdf8279d67711f8adf4
Linggg <bluish@Lingggs-MacBook-Pro.local>
2019-03-10 00:36:55 -0800

    each recipe is splited but with a lot of unuseful content

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 import json
 import requests
 from flask import Flask, request
+from flask_paginate import Pagination, get_page_parameter
 
 
 app = Flask(__name__)
@@ -8,13 +9,19 @@ app_id = '9c6002af'
 app_key = '29d5731c89d6adc4fbba5a8e4bbfb1d9'
 
 @app.route('/')
-def hello():
-	url = 'https://api.edamam.com/search?q=' + food_name + '&app_id=' + app_id + '&app_key=' + app_key
-	#'https://api.edamam.com/search?q=chicken&app_id=9c6002af&app_key=29d5731c89d6adc4fbba5a8e4bbfb1d9&from=0&to=3&calories=591-722&health=alcohol-free'
-	#url = 'https://api.edamam.com/search?q=' + food_name + '&app_id=' + app_id + '&app_key=' + app_key
+def recipeSearch():
+	url = 'https://api.edamam.com/search?q=chicken' + '&app_id=' + app_id + '&app_key=' + app_key
+	# url = 'https://api.edamam.com/search?q=' + food_name + '&app_id=' + app_id + '&app_key=' + app_key
 	r = requests.get(url)
-	return json.dumps(r.json())
+	# results = json.loads(r.text)
+	for x in r.json()['hits']:
+		# y = json.dumps((r.json())['hits'][x])
+		print(x['recipe'])
 
+	return '1'
+
+	#'https://stackoverflow.com/questions/32911336/what-is-the-difference-between-json-dumps-and-json-load'
+	# difference between json.load and json.dump
 
 
 if __name__ == '__main__':
