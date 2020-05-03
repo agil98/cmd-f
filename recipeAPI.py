5f564256837e08b83dd7886927ff304178834810
qlnjq <45474496+qlnjq@users.noreply.github.com>
2020-05-02 20:01:20 -0700

    replace dynamic label variable

--- recipeAPI.py
+++ recipeAPI.py
@@ -9,7 +9,8 @@ app_key = '048cc175d1a6141040a85f9fb7c55cf5'
 
 # @app.route('/choose-recipe/<label>')
 def recipe_search(label):
-    url = 'https://api.edamam.com/search?q=frozen+dessert' + '&app_id=' + app_id + '&app_key=' + app_key + '&from=0&to=4'
+    label.replace(" ", "+")
+    url = 'https://api.edamam.com/search?q=' + label + '&app_id=' + app_id + '&app_key=' + app_key + '&from=0&to=4'
     response = requests.get(url)
     results = json.loads(response.text)
     recipes = results["hits"]
